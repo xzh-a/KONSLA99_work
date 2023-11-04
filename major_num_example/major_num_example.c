233dc660e38c344bf962cd420f43c32294ed3774
mingi-chae <asminkee@gmail.com>
2023-11-04 19:08:19 +0900

    major_num_example by mingi-chae

---
+++ major_num_example/major_num_example.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+        int dev = open("/dev/mydevice",O_RDONLY);
+        if(dev == 1){
+                printf("operating was not possible\n");
+                return -1;
+	}
+        printf("operating was successsful\n");
+        close(dev);
+        return 0;
+}
