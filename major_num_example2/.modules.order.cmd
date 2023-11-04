8f5a46983282ced913b6978970e5c56ce46f77c4
Khunilee <hijordan98@gmail.com>
2023-11-04 03:22:33 -0700

    kihunlee

---
+++ major_num_example2/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/kihunlee/working/major_num_example/modules.order := {   echo /home/kihunlee/working/major_num_example/dev_nr.ko; :; } | awk '!x[$$0]++' - > /home/kihunlee/working/major_num_example/modules.order
