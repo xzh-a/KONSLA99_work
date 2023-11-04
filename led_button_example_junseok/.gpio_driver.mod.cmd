669758d1b1dafe093bbbcd292c6a32f222467749
Junseok <haku3131@naver.com>
2023-11-04 20:47:38 +0900

    JunseokKim

---
+++ led_button_example_junseok/.gpio_driver.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/junseok/working/led_button_example/gpio_driver.mod := { echo  /home/junseok/working/led_button_example/gpio_driver.o;  echo; } > /home/junseok/working/led_button_example/gpio_driver.mod
---
+++ led_button_example_junseok/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/junseok/working/led_button_example/modules.order := {   echo /home/junseok/working/led_button_example/gpio_driver.ko; :; } | awk '!x[$$0]++' - > /home/junseok/working/led_button_example/modules.order
