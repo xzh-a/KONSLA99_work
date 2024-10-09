18cf94921acd277a227ac1b6f741a732e997c4f3
Konsla99 <asminkee@gmail.com>
2024-10-09 19:21:50 +0900

    add 3D gaussian splatting - EAGLES(ECCV2024)- original version

---
+++ efficientgaussian/render.py
@@ -0,0 +1,90 @@
+#
+# Copyright (C) 2023, Inria
+# GRAPHDECO research group, https://team.inria.fr/graphdeco
+# All rights reserved.
+#
+# This software is free for non-commercial, research and evaluation use 
+# under the terms of the LICENSE.md file.
+#
+# For inquiries contact  george.drettakis@inria.fr
+#
+
+import torch
+from scene import Scene
+import os
+from tqdm import tqdm
+from os import makedirs
+from gaussian_renderer import render
+import torchvision
+from utils.general_utils import safe_state
+from argparse import ArgumentParser
+from arguments import ModelParams, PipelineParams, QuantizeParams,get_combined_args
+from gaussian_renderer import GaussianModel
+# 코드 병신이라 추가
+import yaml
+from collections import defaultdict
+import sys
+
+def render_set(model_path, name, iteration, views, gaussians, pipeline, background):
+    render_path = os.path.join(model_path, name, "ours_{}".format(iteration), "renders")
+    gts_path = os.path.join(model_path, name, "ours_{}".format(iteration), "gt")
+
+    makedirs(render_path, exist_ok=True)
+    makedirs(gts_path, exist_ok=True)
+
+    for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
+        rendering = render(view, gaussians, pipeline, background)["render"]
+        gt = view.original_image[0:3, :, :]
+        torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
+        torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
+
+def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParams, quantize: QuantizeParams, 
+                skip_train : bool, skip_test : bool):
+    with torch.no_grad():
+        gaussians = GaussianModel(dataset.sh_degree)
+        scene = Scene(dataset, gaussians, load_iteration=iteration, shuffle=False)
+
+        bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
+        background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
+
+        if not skip_train:
+             render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background)
+
+        if not skip_test:
+             render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background)
+
+if __name__ == "__main__":
+    # # Set up command line argument parser
+    # parser = ArgumentParser(description="Testing script parameters")
+    # model = ModelParams(parser, sentinel=True)
+    # pipeline = PipelineParams(parser)
+    # quantize = QuantizeParams(parser)
+
+    # 수정
+        # Config file is used for argument defaults. Command line arguments override config file.
+    config_path = sys.argv[sys.argv.index("--config")+1] if "--config" in sys.argv else None
+    if config_path:
+        with open(config_path) as f:
+            config = yaml.load(f, Loader=yaml.FullLoader)
+    else:
+        config = {}
+    config = defaultdict(lambda: {}, config)
+
+    parser = ArgumentParser(description="Training script parameters")
+    model = ModelParams(parser, config['model_params'])
+   # op = OptimizationParams(parser, config['opt_params'])
+    pipeline = PipelineParams(parser, config['pipe_params'])
+    quantize = QuantizeParams(parser, config['quantize_params'])
+
+    parser.add_argument('--config', type=str, default=None)
+    parser.add_argument("--iteration", default=-1, type=int)
+    parser.add_argument("--skip_train", action="store_true")
+    parser.add_argument("--skip_test", action="store_true")
+    parser.add_argument("--quiet", action="store_true")
+    args = get_combined_args(parser)
+    print("Rendering " + args.model_path)
+
+    # Initialize system state (RNG)
+    safe_state(args.quiet)
+
+    render_sets(model.extract(args), args.iteration, pipeline.extract(args), quantize.extract(args), args.skip_train, args.skip_test)
