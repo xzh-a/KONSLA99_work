038ab990588b52d39e0f6bfc629fb4c820a16b86
Konsla99 <asminkee@gmail.com>
2024-10-23 15:47:23 +0900

    train result, % measure compare

--- efficientgaussian/run.sh
+++ efficientgaussian/run.sh
@@ -1,7 +1,8 @@
-python ./dynamic_prune_train.py --config ./configs/efficient-3dgs.yaml -s ./custom_data/s_free/ -m ./log_dir/strike_freedom_p/dynamic/ --save_ply >> ./proj/dynamin.txt
+# python ./importance_prune_train.py --config ./configs/efficient-3dgs.yaml -s ./custom_data/s_free/ -m ./log_dir/strike_freedom_p/importance/ --save_ply >> ./proj/importance.txt
 
-python ./mag_prune_train.py --config ./configs/efficient-3dgs.yaml -s ./custom_data/s_free/ -m ./log_dir/strike_freedom_p/magnitude/ --save_ply >> ./proj/mag.txt
+# python ./mag_prune_train.py --config ./configs/efficient-3dgs.yaml -s ./custom_data/s_free/ -m ./log_dir/strike_freedom_p/magnitude/ --save_ply >> ./proj/mag.txt
 
-python ./struc_prune_train.py --config ./configs/efficient-3dgs.yaml -s ./custom_data/s_free/ -m ./log_dir/strike_freedom_p/structured/ --save_ply >> ./proj/struc.txt
+# python ./struc_prune_train.py --config ./configs/efficient-3dgs.yaml -s ./custom_data/s_free/ -m ./log_dir/strike_freedom_p/structured/ --save_ply >> ./proj/struc.txt
 
-python ./train_eval.py --config ./configs/efficient-3dgs.yaml -s ./custom_data/s_free// -m ./log_dir/strike_freedom_p/origin/ --save_ply >> ./proj/origin.txt
+# python ./train_eval.py --config ./configs/efficient-3dgs.yaml -s ./custom_data/s_free// -m ./log_dir/strike_freedom_p/origin/ --save_ply >> ./proj/origin.txt
+
